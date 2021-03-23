a577b2ec18158fa0e744bc0cd4d451e40945c4f2
‘Andrei <andrei.dima@usm.ro>
2021-03-23 21:06:16 +0200

    a

--- resources/views/voteaza_si_propune/create.blade.php
+++ resources/views/voteaza_si_propune/create.blade.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
-<div class="container" id="app1" style="background-color:white">
+<div class="container" id="app1" style="background-color:white !important">
 
     {{-- Topul International --}}
     <div class="row justify-content-center">
